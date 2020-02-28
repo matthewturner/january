979aacad388f0ce8923be9547e047e969b7ab96c
Matt Turner <matt@fiverings.uk>
2020-02-28 19:57:46 +0000

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -3,9 +3,6 @@
 #define IN3 10
 #define IN4 11
 
-int Steps = 0;
-boolean Direction = true;
-
 void setup() {
   Serial.begin(9600);
   pinMode(IN1, OUTPUT);
@@ -14,86 +11,82 @@ void setup() {
   pinMode(IN4, OUTPUT);
 }
 
+int currentStep = 0;
+
 void loop() {
   for (int i = 0; i < 4096; i++) {
-    stepper(1);
+    moveToStep(currentStep);
+    currentStep = incrementStep(currentStep);
     delayMicroseconds(800);
   }
-
-  Direction = !Direction;
+  moveToStep(8);
+  delay(1000);
+  Serial.println(currentStep);
 }
 
-void stepper(int xw) {
-  for (int x = 0; x < xw; x++) {
-    switch (Steps) {
-      case 0:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, HIGH);
-        break;
-      case 1:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, HIGH);
-        break;
-      case 2:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, LOW);
-        break;
-      case 3:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);
-        digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, LOW);
-        break;
-      case 4:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        break;
-      case 5:
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, HIGH);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        break;
-      case 6:
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        break;
-      case 7:
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, HIGH);
-        break;
-      default:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        break;
-    }
-
-    SetDirection();
+void moveToStep(int step) {
+  switch (step) {
+    case 0:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      break;
+    case 1:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, HIGH);
+      break;
+    case 2:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      break;
+    case 3:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      break;
+    case 4:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      break;
+    case 5:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      break;
+    case 6:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      break;
+    case 7:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      break;
+    default:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      break;
   }
 }
 
-void SetDirection() {
-  if (Direction == 1) {
-    Steps++;
-  } if (Direction == 0) {
-    Steps--;
-  } if (Steps > 7) {
-    Steps = 0;
-  } if (Steps < 0) {
-    Steps = 7;
+int incrementStep(int step) {
+  step++;
+  if (step > 7) {
+    return 0;
   }
+  return step;
 }
