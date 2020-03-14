c27008b7c692074e0608402ceb8bf8b8044963e0
Matt Turner <matt@fiverings.uk>
2020-03-14 17:31:15 +0000

    Extract wallet

--- main.ino
+++ main.ino
@@ -2,21 +2,63 @@
 #include <CheapStepper.h>
 
 CheapStepper stepper (4, 5, 6, 7);
-Servo servoFixWallet;
+Servo servoLockWallet;
 Servo servoSplitWallet;
+int triggerPin = 3;
 
 void setup() {
   Serial.begin(9600);
-  servoFixWallet.attach(9);
-  servoSplitWallet.attach(12);
+  pinMode(triggerPin, INPUT); 
+  servoLockWallet.attach(8);
+  servoSplitWallet.attach(9);
+  test();
 }
 
 void loop() {
-  stepper.moveDegrees(true, 360);
-  
-  servoFixWallet.write(0);
-  delay(1000);
+  if(triggered()) {
+    lockWallet();
+    splitWallet();
+    extractEdge();
+    extractWallet();
+    reset();
+  }
+}
 
-  servoFixWallet.write(180);
+bool triggered() {
+  int value = digitalRead(triggerPin);
+  Serial.println(value);
+  return (value == HIGH);
+}
+
+void extractEdge() {
+  // do something clever
+}
+
+void extractWallet() {
+  stepper.moveDegrees(true, 360 * 3.5);
+}
+
+void splitWallet() {
+  servoSplitWallet.write(0);
+  delay(500);
+  servoSplitWallet.write(45);
+  delay(500);
+}
+
+void lockWallet() {
+  servoLockWallet.write(0);
+  delay(500);
+}
+
+void reset() {
+  servoSplitWallet.write(90);
+  servoLockWallet.write(160);
+  delay(500);
+}
+
+void test() {
+  lockWallet();
+  splitWallet();
+  reset();
   delay(1000);
 }
